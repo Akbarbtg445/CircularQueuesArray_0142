5ac22e37dc992f42706a381d78be26cea9467aab
M. Ridho Akbar <129049803+Akbarbtg445@users.noreply.github.com>
2025-06-12 15:41:25 +0700

    Tambahan

--- Queues.cpp
+++ Queues.cpp
@@ -17,13 +17,13 @@ class Queues {
         cin >> num;
         cout << "endl";
 
-        //cek apakah antrian penuh
+        // 1.cek apakah antrian penuh
         if ((FRONT == 0 && REAR == max-1) || (FRONT == REAR +1))
         {
             cout << "\nQueue overflow\n";
             return;
         }
-        // Cek apakah antrian kosong 
+        // 2.Cek apakah antrian kosong 
         if (FRONT == -1)
         {
             FRONT =0;
